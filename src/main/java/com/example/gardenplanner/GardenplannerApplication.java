474140a8eca2677941c30dc12bf205c5db151fdd
ochwada <nakhulolinda@yahoo.com>
2025-07-09 15:31:28 +0200

    initial project setup

---
+++ src/main/java/com/example/gardenplanner/GardenplannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gardenplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gardenplanner/GardenplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gardenplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
