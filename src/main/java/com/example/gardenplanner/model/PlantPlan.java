a39b1d9e741a5f3f2e8884abf588cb52c86c8dda
ochwada <nakhulolinda@yahoo.com>
2025-07-09 16:10:00 +0200

    model package - done

---
+++ src/main/java/com/example/gardenplanner/model/PlantPlan.java
@@ -0,0 +1,53 @@
+package com.example.gardenplanner.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * *******************************************************
+ * Package: com.example.gardenplanner.model
+ * File: PlantPlan.java
+ * Author: Ochwada
+ * Date: Wednesday, 09.Jul.2025, 3:58 PM
+ * Description: Represents a plant care plan stored in the MongoDB collection "plantplans".
+ * Objective:
+ * *******************************************************
+ */
+
+
+/**
+ * Lombok annotations:
+ * - {@code @Data} generates getters, setters, toString, equals, and hashCode.
+ * - {@code @NoArgsConstructor} creates a no-argument constructor.
+ * - {@code @AllArgsConstructor} creates a constructor with all fields as parameters.
+ * MongoDB:
+ * - {@code @Document(collection = "plantplans")} indicates this class maps to the "plantplans" collection.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "plantplans")
+public class PlantPlan {
+    /** Unique identifier for the plant plan.*/
+    @Id
+    private String id;
+
+    /** The name of the plant (e.g., "Tomato", "Rose"). */
+    private String name;
+
+    /** Recommended planting season as an enum. ( MongoDB stores it as a string (e.g., "SPRING", "SUMMER")). */
+    private Season plantingSeason;
+
+    /** Sunlight requirements (e.g., "Full Sun", "Partial Shade"). */
+    private  String sunlightNeeds;
+
+    /** Watering frequency (e.g., "Daily", "Twice a week").*/
+    private String wateringFreq;
+
+    /** Any additional notes or care instructions. */
+    private String notes;
+
+
+}
---
+++ src/main/java/com/example/gardenplanner/model/Season.java
@@ -0,0 +1,22 @@
+package com.example.gardenplanner.model;
+
+
+/**
+ * *******************************************************
+ * Package: com.example.gardenplanner.model
+ * File: Season.java
+ * Author: Ochwada
+ * Date: Wednesday, 09.Jul.2025, 4:05 PM
+ * Description: Represents the seasons of the year relevant for planting.
+ * - This enum can be used to specify the planting season for a {@link PlantPlan}.
+ * Objective:
+ * *******************************************************
+ */
+
+
+public enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+}
